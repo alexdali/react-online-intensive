6fd1daa6bb366d828a923d9f37fe95250b058189
alexeytas <alexdali@gmail.com>
2018-11-27 13:24:16 +0600

    added Spinner

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -5,18 +5,34 @@ import React, { Component } from 'react';
 import StatusBar from 'components/StatusBar';
 import Composer from 'components/Composer';
 import Post from 'components/Post';
+import Spinner from 'components/Spinner';
 
 // Instruments
 import Styles from './styles.m.css';
 
 export default class Feed extends Component {
+    state = {
+        posts: [
+            { id: '123', comment: 'Hi there', created: 1526825071433 },
+            { id: '456', comment: 'Hi there again', created: 1526825079433 },
+        ],
+        isSpinning: true,
+    };
+
     render() {
+        const { posts, isSpinning } = this.state;
+
+        const postsJSX = posts.map((post) => {
+            return <Post key = { post.id } { ...post }/>;
+        });
+
         return (
             <>
                 <section className = { Styles.feed }>
+                    <Spinner isSpinning = { isSpinning } />
                     <StatusBar />
                     <Composer />
-                    <Post />
+                    {postsJSX}
                 </section>
             </>
         );
