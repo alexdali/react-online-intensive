881f7f0578ef34779323f7e25bcede0a551ccc68
alexeytas <alexdali@gmail.com>
2018-12-04 22:06:58 +0600

    added WebSocket for Like/Unlike

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -52,11 +52,27 @@ export default class Feed extends Component {
                 }));
             }
         });
+
+        socket.on('like', (postJSON) => {
+            const { data: likedPost, meta } = JSON.parse(postJSON);
+
+            if (
+                `${currentUserFirstName} ${currentUserLastName}`
+                !==`${meta.authorFirstName} ${meta.authorLastName}`
+            ) {
+                this.setState(({ posts }) => ({
+                    posts: posts.map((post) => {
+                        post.id === likedPost.id ? likedPost : post; 
+                    }),
+                }));
+            }
+        });
     }
 
     componentWillMount () {
         socket.removeListener('create');
         socket.removeListener('remove');
+        socket.removeListener('like');
     }
 
     _setPostsFetchingState = (state) => {
